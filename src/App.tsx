276aefa6cb9b3e73113573f70b7cef6328fd0e58
Seu nome <yago_bohmcruz@hotmail.com>
2024-09-18 11:55:12 -0300

    Create Login Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,45 @@
+import { FaFacebookF, FaApple, FaXbox } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
+import ritoGomes from "../public/ritogomes.png";
+import jinx from "../public/jinx-wallpaper.jpg";
 import "./App.css";
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <h1>React Typescript Template</h1>
-      </header>
+    <div className='login-page'>
+      <aside className='login-form'>
+        <img src={ritoGomes} alt='riot-logo' />
+        <h1>Sign In</h1>
+        <form>
+          <input type='text' placeholder='USERNAME' />
+          <input type='password' placeholder='PASSWORD' />
+          <div className='stay-signed-in'>
+            <input className='check-input' type='checkbox' />
+            <label>Stay signed in</label>
+          </div>
+          <button type='submit'>Sign In</button>
+        </form>
+        <div className='social-login'>
+          <button className='social-button facebook'>
+            <FaFacebookF />
+          </button>
+          <button className='social-button google'>
+            <FcGoogle />
+          </button>
+          <button className='social-button apple'>
+            <FaApple />
+          </button>
+          <button className='social-button xbox'>
+            <FaXbox />
+          </button>
+        </div>
+        <p>
+          Can't sign in? <a href='#'>Create Account</a>
+        </p>
+      </aside>
+      <section className='login-background'>
+        <img src={jinx} alt='login background' />
+      </section>
     </div>
   );
 }
